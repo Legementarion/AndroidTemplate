d2cf08fb67bf90d73bfa9c2ec4ac4fc9780092a4
Lego <dmitry.kovalenko@yalantis.com>
2019-01-02 17:54:19 +0200

    + more ext

--- sources/base/injector/src/main/java/com/kotlinsg/kworkshopapp/tools/ImageVIewExtensions.kt
+++ sources/base/injector/src/main/java/com/kotlinsg/kworkshopapp/tools/ImageVIewExtensions.kt
@@ -4,9 +4,18 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-fun ImageView.showImage(image: String, options: RequestOptions) {
+fun ImageView.showImage(image: String, options: RequestOptions = RequestOptions()) {
     Glide.with(context)
         .load(image)
         .apply(options)
         .into(this@showImage)
+}
+
+fun ImageView.showCircleImage(image: String, tempOptions: RequestOptions = RequestOptions()) {
+    var options: RequestOptions = tempOptions
+    options = options.circleCrop()
+    Glide.with(context)
+        .load(image)
+        .apply(options)
+        .into(this)
 }

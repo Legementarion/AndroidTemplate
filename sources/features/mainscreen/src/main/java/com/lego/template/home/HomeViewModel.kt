4437580e95360cd812ed9e8e7b0a21ad3c1f3fe6
Legementarion <mr.re66it@gmail.com>
2019-05-09 17:41:46 +0300

    base nav

--- sources/features/mainscreen/src/main/java/com/lego/template/home/HomeViewModel.kt
+++ sources/features/mainscreen/src/main/java/com/lego/template/home/HomeViewModel.kt
@@ -2,5 +2,14 @@ package com.lego.template.home
 
 import com.lego.template.base.mvvm.BaseViewModel
 
-class HomeViewModel: BaseViewModel() {
+class HomeViewModel : BaseViewModel() {
+
+    fun moveToMaps() = navigate(HomeFragmentDirections.actionHomeFragmentToMapScreenFragment())
+
+
+    fun moveToRecycler() = navigate(HomeFragmentDirections.actionHomeFragmentToRecycleScreenFragment())
+
+
+    fun moveToChat() = navigate(HomeFragmentDirections.actionHomeFragmentToChatScreenFragment())
+
 }
